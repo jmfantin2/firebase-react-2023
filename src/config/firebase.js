33907302b3d819da2201e8d6d1f299d63a367efd
JM <jmfantin@JMs-MacBook-Pro.local>
2023-06-30 15:27:04 -0300

    mail sign in

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,15 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbHziWfuSpeukMMayo53KZFlSL_XNIJQw",
-  authDomain: "fir-react-2023.firebaseapp.com",
-  projectId: "fir-react-2023",
-  storageBucket: "fir-react-2023.appspot.com",
-  messagingSenderId: "598866252399",
-  appId: "1:598866252399:web:cd323da7858348087af39a",
-  measurementId: "G-TX33SBGHL5"
+  apiKey: 'AIzaSyDbHziWfuSpeukMMayo53KZFlSL_XNIJQw',
+  authDomain: 'fir-react-2023.firebaseapp.com',
+  projectId: 'fir-react-2023',
+  storageBucket: 'fir-react-2023.appspot.com',
+  messagingSenderId: '598866252399',
+  appId: '1:598866252399:web:cd323da7858348087af39a',
+  measurementId: 'G-TX33SBGHL5'
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
