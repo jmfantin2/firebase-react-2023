4281193dcafafc6d72860da0274bc52c5b9d3aae
JM <jmfantin@JMs-MacBook-Pro.local>
2023-06-30 13:19:30 -0300

    firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbHziWfuSpeukMMayo53KZFlSL_XNIJQw",
+  authDomain: "fir-react-2023.firebaseapp.com",
+  projectId: "fir-react-2023",
+  storageBucket: "fir-react-2023.appspot.com",
+  messagingSenderId: "598866252399",
+  appId: "1:598866252399:web:cd323da7858348087af39a",
+  measurementId: "G-TX33SBGHL5"
+};
+
+const app = initializeApp(firebaseConfig);
